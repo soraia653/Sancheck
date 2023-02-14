4b8226af0e808fe5f965955926e95e7b9304df40
Soraia Tavares <76966637+soraia653@users.noreply.github.com>
2023-02-14 10:36:54 +0100

    Update settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'https://sancheck.up.railway.app/',
+    'sancheck.up.railway.app',
     'localhost',
     'https://soraia653-zany-system-rxrpxvw995w3p9v9-8000.preview.app.github.dev/'
 ]
@@ -147,7 +147,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://sancheck.up.railway.app/',
+    'sancheck.up.railway.app',
     'https://soraia653-zany-system-rxrpxvw995w3p9v9-8000.preview.app.github.dev/'
 ]
 
